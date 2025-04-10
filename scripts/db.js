10039cb3c6c75b903abe5219cf096d34379bd8b4
Malte Rettberg <malte.rettberg@web.de>
2025-04-10 16:21:31 +0200

    added databases

---
+++ scripts/db.js
@@ -0,0 +1,62 @@
+let mainDishes = [
+  {
+    "name": "Spaghetti Carbonara",
+    "price": 12.99,
+    "description":
+      "Hausgemachte Spaghetti mit Guanciale, Ei, Pfeffer und Parmesan",
+  },
+  {
+    "name": "Spaghetti al Pomodoro",
+    "price": 10.99,
+    "description": "Hausgemachte Spaghetti unserer berühmten Tomatensauce",
+  },
+  {
+    "name": "Papardelle al Limone",
+    "price": 11.99,
+    "description": "Hausgemachte Papardelle mit Pistazien und Pecorino",
+  },
+  {
+    "name": "Tagliatelle al ragù bolognese",
+    "price": 15.99,
+    "description":
+      "Hausgemachte Tagliatelle mit Rinderhack, Genüse und Tomaten",
+  },
+  {
+    "name": "Lasagne alla Casa",
+    "price": 15.99,
+    "description":
+      "Hausgemachte klassische Lasagne mit Ragù, Béchamel und viel Käse",
+  },
+  {
+    "name": "Penne Gorgonzola",
+    "price": 11.99,
+    "description":
+      "Hausgemachte Penne in einer Gorganzola-Tomatensauce mit vielen Kräutern",
+  },
+];
+
+let sideDishes = [
+  {
+    "name": "Caprese",
+    "price": 8.99,
+    "description": "Salat aus Tomaten, Mozzarella, Basilikum und Olivenöl",
+  },
+  {
+    "name": "Bruschetta",
+    "price": 7.99,
+    "description":
+      "Geröstetes Baguette mit Tomaten, Basilikum, Olivenöl und Knoblauch",
+  },
+  {
+    "name": "Minestrone",
+    "price": 6.99,
+    "description":
+      "Hausgemachte italienische Gemüsesuppe mit kleinen Nudeln und etwas Parmesan",
+  },
+  {
+    "name": "Panzanella",
+    "price": 11.99,
+    "description":
+      "Italienischer Brotsalat mit bunten Tomaten, Zwiebeln, Avocado und unserem Knusperbaguette",
+  },
+];
